d9d926e97103e3864d53c0a376cff87c405fd2f4
kira1009 <cage1009@hotmail.com>
2016-12-06 20:01:13 -0500

    fix tag function

--- html/home.php
+++ html/home.php
@@ -9,48 +9,62 @@ session_start();
     <link rel="stylesheet" href="../css/common.css" type="text/css"/>
     <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css"/>
     <link rel="stylesheet" href="../css/home.css" type="text/css"/>
-    <script src="../js/jquery-3.1.1.min.js"/>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $("#tag").click(function(){
-                $("#tags").toggle();
-            });
-        });
+    <script src="../js/jquery-3.1.1.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $("#tags").hide();
+        })
     </script>
 </head>
 <body>
-<div class="body">
-    <?php
-        if($_SESSION['username'] == null) {
-            echo "<script>window.location.href = './login.php';</script>";
-        }
-    ?>
-    <div class="header">
-        <div id="goHome">
-            <a href="home.php" style="text-decoration: none;">CookZilla</a>
-        </div>
-        <form id="search" method="post" action="../php/search.php" enctype="multipart/form-data">
-        <div class="search">
-                <div><input id="searchText" name="search" type="text" placeholder="Find a recipe" style="color: grey;"></div>
-                <div id="tag" style="cursor:pointer;">Tag<span class="caret"></span></div>
-                <div style="margin-top: 3px;"><button class="btn btn-default">Search</button></div>
-        </div>
-        <div id="username"><?php echo $_SESSION['username'];?></div>
-        <div id="tags" class="tags">
-            <?php
-            require '../php/db_query.php';
-            $count = 1;
-            foreach (getTags() as $element) {
-                echo "<div id='tagOption".$count."' style='width: 30%'><input type='checkbox' name=selectedTag[] value=".$element['tname']." style='width: 20px'/><label>".$element['tname']."</label></div>";
-                $count = $count + 1;
+    <div class="body">
+        <?php
+            if($_SESSION['username'] == null) {
+                echo "<script>window.location.href = './login.php';</script>";
             }
-            ?>
-            <div id="addTag" class="btn btn-default" style="float: right">Cancel</div>
-            <div id="cancelTag" class="btn btn-primary" style="float: right; margin-right: 3px;">OK</div>
+        ?>
+        <div class="header">
+            <div id="goHome">
+                <a href="home.php" style="text-decoration: none;">CookZilla</a>
+            </div>
+            <form id="search" method="post" action="../php/search.php" enctype="multipart/form-data">
+                <div class="search">
+                    <div>
+                        <input id="searchText" name="search" type="text" placeholder="Find a recipe" style="color: grey;">
+                    </div>
+                    <div id="tag" style="cursor:pointer;">
+                        Tag<span class="caret"></span>
+                    </div>
+                    <div style="margin-top: 3px;">
+                        <button class="btn btn-default">Search</button>
+                    </div>
+                </div>
+                <div id="username">
+                    <?php echo $_SESSION['username'];?>
+                </div>
+                <script type="text/javascript">
+                    $("#tag").click(function(){
+                        $("#tags").toggle();
+                    });
+                </script>
+                <div id="tags" class="tags">
+                    <?php
+                    require '../php/db_query.php';
+                    $count = 1;
+                    foreach (getTags() as $element) {
+                       echo "<div id='tagOption".$count."' style='width: 30%'><input type='checkbox' name=selectedTag[] value=".$element['tname']." style='width: 20px; float:left;'/><label>".$element['tname']."</label></div>";
+                      $count = $count + 1;
+                    }
+                    ?>
+                    <div id="addTag" class="btn btn-primary" style="float: right; margin-right: 3px;">OK</div>
+                    <script>
+                        $("#addTag").click(function () {
+                            $("#tags").hide();
+                        });
+                    </script>
+                </div>
+            </form>
         </div>
-
-        </form>
-    </div>
     </div>
 
     <div class="container">
