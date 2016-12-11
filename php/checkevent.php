fc69970e2b7ebd7a2763cb2fa34158995da55ed6
weishi93 <ws1196@nyu.edu>
2016-12-11 17:16:37 -0500

    update checkevent.php

--- php/checkevent.php
+++ php/checkevent.php
@@ -59,6 +59,33 @@ if ($response[0] == true) {
     $response[1] = "Successfully create your event!";
     //process return
 }
+
+// insert user into EventRSVP table
+if ($response[0] == true) {
+    if (!($stmt = $connection->prepare("INSERT INTO EventRSVP (eid, uname) VALUES (?, ?)"))) {
+        $response[0] = false;
+        $response[1] = 'DB statement prepare error';
+        $stmt->close();
+        return $response;
+    }
+    if(!$stmt->bind_param('is', $thisEid, $_SESSION['username'])) {
+        $response[0] = false;
+        $response[1] = 'DB parameter bind error';
+        $stmt->close();
+        return $response;
+    }
+    if(!$stmt->execute()) {
+        $response[0] = false;
+        $response[1] = 'DB statement execute error';
+        $stmt->close();
+        return $response;
+    }
+    $stmt->close();
+    $response[0] = true;
+    $response[1] = "Successfully create your event!";
+    //process return
+}
+
 echo "<script>alert('".$response[1]."');</script>";
 if($response[0] == true) {
     echo "<script>window.location.href = '../html/home.php';</script>";
