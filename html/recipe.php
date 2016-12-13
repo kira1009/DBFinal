c1b266803267a7928d4779afb4deb9cddf9feee9
kira1009 <cage1009@hotmail.com>
2016-12-12 19:26:56 -0500

    add detail page and review function review presentation and review function is not implemented.

--- html/recipe.php
+++ html/recipe.php
@@ -13,11 +13,19 @@ $basicInfo = getRecipeInfoById($rid);
     <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css"/>
     <link rel="stylesheet" href="../css/home.css" type="text/css"/>
     <link rel="stylesheet" href="../css/recipe.css" type="text/css"/>
-    <script src="../js/jquery-3.1.1.min.js"></script>
+    <link rel="stylesheet" href="../css/nivo-slider.css" type="text/css"/>
+    <link rel="stylesheet" href="../css/default.css" type="text/css"/>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js" type="text/javascript"></script>
+    <script src="../js/jquery.nivo.slider.js"></script>
     <script>
         $(document).ready(function () {
             $("#tags").hide();
         })
+    </script>
+    <script type="text/javascript">
+        $(window).load(function () {
+            $('#slider').nivoSlider();
+        });
     </script>
 </head>
 <body>
@@ -73,10 +81,75 @@ $basicInfo = getRecipeInfoById($rid);
     <div class="container">
         <div class="blog-header">
             <h1 class="blog-title" style="font-weight: bold"><?php echo $basicInfo[0]['rtitle'] ?></h1>
-            <p class="lead blog-description"></p>
+            <hr style="margin-bottom: auto">
+            <p style="color: grey"><?php echo $basicInfo[0]['serving']." servings";?></p>
+            <?php
+                $tags = getRecipeTagById($rid);
+                if (empty($tags)) {
+                    echo "<p style='color: grey; margin-top: -10px'>no tag</p>";
+                }else {
+                    $showTag = "";
+                    foreach ($tags as $tag) {
+                        $showTag = $showTag."#".$tag['tname']."\t";
+                    }
+                    echo "<p style='color: grey;margin-top: -10px;'>".$showTag."</p>";
+                }
+            ?>
         </div>
-        <h2 style="font-weight: bold"></h2>
 
+        <div class="theme-default" style="min-height: 400px; width: 100%;">
+            <div style="float: left;width: 60%;">
+                <h2>Ingredients</h2>
+                <hr style="width: 90%; margin-top: auto;float: left">
+                <br>
+                <?php
+                    $ingredients = getRecipeIngredientById($rid);
+                    foreach ($ingredients as $ingredient) {
+                        $amount = $ingredient['amount'];
+                        $unit = $ingredient['unit'];
+                        $name = $ingredient['iname'];
+                        echo "<p>".$amount." ".$unit." of ".$name."</p>";
+                    }
+                ?>
+            </div>
+            <div id="slider" class="nivoSlider">
+                <?php
+                    $images = $basicInfo[0]['rimage'];
+                    $imgDirs = explode(';', $images);
+                    foreach ($imgDirs as $imgDir) {
+                        echo "<img src='".$imgDir."' alt='no image' onerror=\"this.src='../img/default.jpg'\"/>";
+                    }
+                ?>
+            </div>
+        </div>
+        <div class="description">
+            <h2>Description</h2>
+            <hr>
+            <p>
+                <?php echo $basicInfo[0]['rtext'];?>
+            </p>
+        </div>
+        <div class="review">
+            <h2>Reviews</h2>
+            <hr>
+            <?php
+                $reviews = getRecipeReviewById($rid);
+                if(empty($reviews)) {
+                    echo "<p>Currently no review, be the first one to comment on this</p>";
+                }else {
+                    foreach ($reviews as $review) {
+                        $htmlContent = "<div class='col-lg-4'><h4>".$review['rrtitle']."</h4>";
+                    }
+                }
+            ?>
+        </div>
+        <div class="comment">
+            <h2>Add your comment</h2>
+            <hr>
+            <form method="post" action="../php/submit_review.php" enctype="multipart/form-data">
+                <input autofocus="autofocus" maxlength="32" minlength="1" name="title" placeholder="Username" type="text" required class="formInput">
+            </form>
+        </div>
     </div>
     <div class="footer">
         <div>Copyright &copy; CookZilla TM. All Right Reserved.</div>
---
+++ php/submit_review.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cagekira
+ * Date: 12/12/16
+ * Time: 7:02 PM
+ */
